cc1665f9ab5f789eab9cdd43d7884bdd1ae39a4c
Galaxeo <hwjustincheok@gmail.com>
2023-04-03 15:18:59 -0400

    feat: case by case redo 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
-
+const sumAll = function(first, second) {
+  let sum = 0;
+  if (first < 0) {
+    return 'ERROR';
+  }
+  if (typeof (second) != 'number') {
+    return 'ERROR';
+  }
+  if (second > first) {
+    while (first <= second) {
+      sum += first;
+      first += 1;
+    }
+  } else if (first > second) {
+    while (second <= first) {
+      sum += second;
+      second += 1;
+    }
+  }
+  return sum;
 };
 
 // Do not edit below this line
