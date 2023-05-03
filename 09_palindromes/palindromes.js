c697f44fc488933bd0b72b09c06c1462b04726e8
Galaxeo <hwjustincheok@gmail.com>
2023-05-03 11:14:37 -0400

    feat: redo 9 palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,19 @@
-const palindromes = function () {
-
+const palindromes = function(str) {
+  str = str.toLowerCase();
+  str = str.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "");
+  str = str.replace(/\s+/g, "");
+  let left = 0;
+  let right = str.length - 1;
+  while (left < right) {
+    if (str[left] != str[right]) {
+      console.log(str, str[left], str[right]);
+      return false;
+    } else {
+      left += 1;
+      right -= 1;
+    }
+  }
+  return true;
 };
 
 // Do not edit below this line
