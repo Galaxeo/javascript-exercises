15d93bbfcf2c5ba9b8ab953769cd606c4b75513a
Galaxeo <hwjustincheok@gmail.com>
2023-05-03 23:17:34 -0400

    feat: redo 12 complete

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,27 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+  function Older(a, b) {
+    let aAge;
+    let bAge;
+    if (a.yearOfDeath & b.yearOfDeath) {
+      aAge = a.yearOfDeath - a.yearOfBirth;
+      bAge = b.yearOfDeath - b.yearOfBirth;
+    }
+    if (!a.yearOfDeath) {
+      console.log("HERE");
+      const curr = new Date();
+      aAge = curr.getFullYear() - a.yearOfBirth;
+      bAge = b.yearOfDeath - b.yearOfBirth;
+    }
+    if (!b.yearOfDeath) {
+      const curr = new Date();
+      bAge = curr.getFullYear() - b.yearOfBirth;
+      aAge = a.yearOfDeath - a.yearOfBirth;
+    }
+    return (aAge > bAge ? a : b);
+  }
+  return arr.reduce((oldest, person) => {
+    return (Older(oldest, person));
+  });
 };
 
 // Do not edit below this line
